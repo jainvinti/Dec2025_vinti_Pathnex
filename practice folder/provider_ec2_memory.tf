5d79cd7a5c33708640069c08fb54b5d3f6b4fddd
Vinti Jain <vinti.online@gmail.com>
2026-02-12 07:53:58 +0530

    Day 3

--- practice folder/provider_ec2_memory.tf
+++ practice folder/provider_ec2_memory.tf
@@ -11,3 +11,33 @@ resource "aws_instance" "ec2_instance" {
         Owner = "vinti"
     }
 }
+
+#-------------------------------------------------------------
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_security_group" "FIS_SG" {
+    name = "FIS_SG"
+    description = "FIS security group"
+
+    ingress = {                   #Inbound rule: who can connect and how.
+        from_port = 22
+        to_port = 22
+        protocol = tcp
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_instance" "FIS_ec2" {
+    ami = "ajhdflakjhdsf"
+    instance_type = "r6i.xlarge"    #r6i.4xlarge (Intel, 16 vCPUs, 128 GiB RAM).
+
+    #vpc_security_group_ids = [aws_security_group.FIS_SG.id] ties the EC2 instance to the security group.
+    vpc_security_group_ids = [aws_security_group.FIS_SG.id]
+
+    tags = {
+      Name = "FIS_EC2"
+    }
+}
--- practice folder/shell.sh
+++ practice folder/shell.sh
@@ -2,4 +2,12 @@
 echo "Date = $date"
 echo "Hostname = $hostname"
 
+FILE="/tmp/logs.txt"
+
+if [ -f "$FILE" ]; then
+    echo "File exists"
+else
+    echo "File doesn't exists"
+fi
+
 df -h  #disk usage
