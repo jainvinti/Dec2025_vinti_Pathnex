14ab7f746a77979aa16d2aff7a215a23c1124166
Vinti Jain <vinti.online@gmail.com>
2026-02-15 01:51:40 +0530

    day 4

--- practice folder/provider_ec2_compute.tf
+++ practice folder/provider_ec2_compute.tf
@@ -14,3 +14,21 @@ resource "aws_instance" "vintiEC2" {
 
 #-------------------------------------------------------------
 
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_key_pair" "vinti_key" {
+  key_name = "Vinti_Key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDf8s"
+}
+
+resource "aws_instance" "vinti_ec2" {
+  ami = "ami-0asdhfluuD"
+  instance_type = "c5.xlarge"
+  key_name = aws_key_pair.vinti_key.key_name
+
+  tags = {
+    Name = "vinti-EC2"
+  }
+}
